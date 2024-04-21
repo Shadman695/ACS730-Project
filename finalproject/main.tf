57da0f5f2e777b4c754abebbb9a84b887ff0bccc
Shadman695 <shadmankhan95@gmail.com>
2024-04-21 05:52:27 +0000

    2nd VPC added

--- finalproject/main.tf
+++ finalproject/main.tf
@@ -1,15 +1,15 @@
 # Define provider (AWS in this case)
 provider "aws" {
-  region = "your_aws_region"
+  region = "us-east-1"
 }
 
-# Configure the backend to store the Terraform state in the existing S3 bucket
+# Configure the backend to store the Terraform state in the main S3 bucket
 terraform {
   backend "s3" {
     bucket         = "finalprojectssk"
-    key            = "terraform.tfstate"
+    key            = "main/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform_locks" # Optionally, if you want to use DynamoDB for locking
+    # dynamodb_table = "terraform_locks" # Optionally, if you want to use DynamoDB for locking
   }
 }
 
@@ -32,4 +32,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index + 3) # Start from the 4th subnet
   availability_zone = "us-east-1b"
+}
+output "main_vpc_id" {
+  value = aws_vpc.main.id
 }
